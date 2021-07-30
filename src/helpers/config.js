1b282483495207cd4d9f9a3bc8e9e564a9a725df
Space Polux <spacepoluxmanager@gmail.com>
2021-07-30 01:47:40 -0500

    Dotenv config

--- src/helpers/config.js
+++ src/helpers/config.js
@@ -1,5 +1,5 @@
-import {config} from 'dotenv';
-config();
+import dotenv from 'dotenv';
+dotenv.config();
 
 export default {
     mongodbURL: process.env.MONGODB_URI || 'mongodb://localhost/rveapi'
