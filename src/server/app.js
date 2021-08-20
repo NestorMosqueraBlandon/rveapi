5efd2db05a054b42614f6949dd42199187681c3a
Space Polux <spacepoluxmanager@gmail.com>
2021-08-19 19:30:16 -0500

    admin img

--- src/server/app.js
+++ src/server/app.js
@@ -13,9 +13,7 @@ const app = express();
 app.set('port', config.PORT);
 
 // middlewares
-app.use(cors({
-    origin: "http://localhost:3000"
-}));
+app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
