d453e8f80f9631ea1fc22a12c5e25d9235a0bb87
Space Polux <spacepoluxmanager@gmail.com>
2021-09-13 15:13:34 -0500

    computers

---
+++ src/controllers/computerController.js
@@ -0,0 +1,48 @@
+import Computer from '../models/Computer.js';
+import getPagination from '../libs/getPagination.js';
+import { generateToken } from '../libs/utils.js';
+
+export const findAllComputers = async (req, res) => {
+  try {
+    const { size, page } = req.query;
+    const { limit, offset } = getPagination(page, size);
+
+    const computers = await Computer.find({});
+    res.json({
+      // totalItems: clients.totalDocs,
+      computers: computers,
+      // totalPages: clients.totalPages,
+      // currentPage: clients.page - 1
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: error.meesage || 'Something goes wrong retrieving the users',
+    });
+  }
+};
+
+export const createComputer = async (req, res) => {
+  const newComputer = new Computer({
+    name: req.body.name,
+    brand: req.body.brand,
+    category: req.body.category,
+    image: req.body.image,
+    price: req.body.price,
+  });
+  const computerCreated = await newComputer.save();
+  res.json(computerCreated);
+};
+
+// export const findOneUser = async(req, res) =>{
+//     const user = await User.findById(req.params.id);
+//     res.json(user);
+// }
+
+export const deleteComputer = async (req, res) => {
+  const computer = await Computer.findByIdAndDelete(req.params.id);
+  res.json({
+    message: 'Computer were deleted successfully',
+  });
+};
+
+// AKIAVHJK4GQ6BBLS6M62
--- src/server/app.js
+++ src/server/app.js
@@ -1,10 +1,11 @@
-import express from 'express'
+import express from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
 import userRouter from '../routes/userRouter.js';
 import adminRouter from '../routes/adminRouter.js';
 import clientRouter from '../routes/clientRouter.js';
 import productRouter from '../routes/productRouter.js';
+import computerRouter from '../routes/computerRouter.js';
 import categoryRouter from '../routes/categoryRouter.js';
 import uploadRouter from '../routes/uploadRouter.js';
 import config from '../helpers/config.js';
@@ -19,12 +20,12 @@ app.set('port', config.PORT);
 app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
-app.use(fileUpload())
+app.use(express.urlencoded({ extended: false }));
+app.use(fileUpload());
 // Routes
 
 app.get('/', (req, res) => {
-    res.send('Server is ready');
+  res.send('Server is ready');
 });
 
 app.use('/api/v1/users', userRouter);
@@ -34,6 +35,8 @@ app.use('/api/v1/admins', adminRouter);
 app.use('/api/v1/clients', clientRouter);
 
 app.use('/api/v1/products', productRouter);
+
+app.use('/api/v1/computers', computerRouter);
 
 app.use('/api/v1/categories', categoryRouter);
 
