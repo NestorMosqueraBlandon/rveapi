856c0e3001fac5dc8ed0c0a45e0e8abeb6553e01
Space Polux <spacepoluxmanager@gmail.com>
2021-09-15 17:43:36 -0500

    apirest

--- src/helpers/config.js
+++ src/helpers/config.js
@@ -6,13 +6,16 @@ import { fileURLToPath } from 'url';
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 dotenv.config({
-    path: path.resolve(__dirname, process.env.NODE_ENV + '.env')
+  path: path.resolve(__dirname, process.env.NODE_ENV + '.env'),
 });
 
 export default {
-    NODE_ENV: process.env.NODE_ENV || 'development',
-    PORT: process.env.PORT || 4000,
-    mongodbURL: process.env.MONGODB_URI || "mongodb+srv://user:api12345@cluster0.pcisi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-    accessKeyId: process.env.accessKeyId || 'AKIAVHJK4GQ6BBLS6M62',
-    secretAccessKey: process.env.secretAccessKey || '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'
-}
+  NODE_ENV: process.env.NODE_ENV || 'development',
+  PORT: process.env.PORT || 4000,
+  mongodbURL:
+    process.env.MONGODB_URI ||
+    'mongodb+srv://user:api12345@cluster0.2tqpl.mongodb.net/apirest?retryWrites=true&w=majority',
+  accessKeyId: process.env.accessKeyId || 'AKIAVHJK4GQ6BBLS6M62',
+  secretAccessKey:
+    process.env.secretAccessKey || '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',
+};
